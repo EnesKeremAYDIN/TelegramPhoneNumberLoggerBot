d5d11f690030856f48565b97fff19382dbfa43bf
Enes Kerem AYDIN <46195766+EnesKeremAYDIN@users.noreply.github.com>
2024-07-01 03:13:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,35 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+const token = "";
+const bot = new TelegramBot(token, { polling: true });
+
+console.log('Bot started.');
+
+bot.onText(/^\/start/, function (msg, match) {
+    var option = {
+        "parse_mode": "Markdown",
+        "reply_markup": {
+            "one_time_keyboard": true,
+            "keyboard": [[{
+                text: "My phone number",
+                request_contact: true
+            }]]
+        }
+    };
+    bot.sendMessage(msg.chat.id, `Hi! Send me your phone number.`, option).then(() => {
+        bot.once('contact', (msg) => {
+            const phoneNumber = msg.contact.phone_number;
+            const username = msg.from.username;
+            const firstname = msg.from.first_name;
+            const lastname = msg.from.last_name;
+
+            console.log(`User: ${firstname} ${lastname} (${username}), Phone Number: ${phoneNumber}`);
+
+            bot.sendMessage(msg.chat.id, `Thanks.`, {
+                reply_markup: {
+                    remove_keyboard: true
+                }
+            });
+        });
+    });
+});
